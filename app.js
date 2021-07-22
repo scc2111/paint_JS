719e4eb4324384f19ed58a49bca3522e4e9f3bf6
scc2111 <scc2111@naver.com>
2021-07-23 06:03:25 +0900

    add filling mode, saving the image

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const eraser = document.getElementById("eraser");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+const eraserColor = "white";
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
 // canvas = html5 element
 // context = canvas 안에서 픽셀을 다루는것
-ctx.strokeStyle = "#2c2c2c"; // 선 색깔
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR; // 선 색깔
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; // 브러쉬 굵기
 
 let painting = false;
@@ -38,6 +47,11 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color; // 위의 strokeStyle color 오버라이딩
+  ctx.fillStyle = color;
+}
+function handleEraserClick(event) {
+  const color = eraserColor;
+  ctx.strokeStyle = color; // 위의 strokeStyle color 오버라이딩
 }
 
 function handleRangeChange(event) {
@@ -55,11 +69,26 @@ function handleModeClick() {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS"; //  download = a의 attribute, 이동 대신 다운
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting); // 클릭
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
@@ -72,4 +101,12 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (eraser) {
+  eraser.addEventListener("click", handleEraserClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
